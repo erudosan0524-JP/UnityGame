3c233b4e22bdde7194d741000af9c306debaf417
erudo <hashierudo@gmail.com>
2018-08-08 20:13:40 +0900

    Add files via upload

---
+++ practice/New 2D Game 1/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	public Rigidbody2D _Rigidbody2D;
+	public LayerMask _GLayer;
+	
+	public Transform _LGroundCheck;
+	public Transform _RGroundCheck;
+
+	public float Speed = 0f;
+	public float Jump = 0f;
+	public bool grounded = false;
+
+	void Start(){
+		_Rigidbody2D = GetComponent<Rigidbody2D>();
+
+		_LGroundCheck = transform.Find("LGroundCheck");
+		_RGroundCheck = transform.Find("RGroundCheck");
+
+	}
+
+	void Update(){
+
+		grounded = Physics2D.Linecast(_LGroundCheck.transform.position, _RGroundCheck.transform.position,_GLayer);
+		float x = Input.GetAxisRaw("Horizontal");
+		_Rigidbody2D.velocity = new Vector2(Speed * x, _Rigidbody2D.velocity.y);
+
+		if(Input.GetKeyDown("space") && grounded){
+
+			_Rigidbody2D.velocity = new Vector2(_Rigidbody2D.velocity.x,Jump);
+		}
+
+		
+	}
+}
